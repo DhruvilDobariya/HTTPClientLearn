7943512dc9752703359276cc692944e6011423ae
Dhruvil Dobariya <190540107051@darshan.ac.in>
2023-02-23 08:53:22 +0530

    Create Backup of HTTPClientLearn

---
+++ HTTPClientDemo/HTTPClientDemo/HTTPClientProgram.cs
@@ -0,0 +1,35 @@
+﻿using Newtonsoft.Json;
+
+
+namespace HTTPClientDemo
+{
+    public class HTTPClientProgram
+    {
+        public static void Main(string[] args)
+        {
+            HttpClient httpClient = new HttpClient();
+            httpClient.BaseAddress = new Uri("https://jsonplaceholder.typicode.com/");
+
+            HttpResponseMessage responce = httpClient.GetAsync("todos").Result;
+            if (responce.IsSuccessStatusCode)
+            {
+                string todo = responce.Content.ReadAsStringAsync().Result.ToString();
+                //Console.WriteLine(todo);
+                List<Todo> todo1 = JsonConvert.DeserializeObject<List<Todo>>(todo.ToString());
+                foreach (var item in todo1)
+                {
+                    Console.WriteLine(item.userId);
+                    Console.WriteLine(item.id);
+                    Console.WriteLine(item.title);
+                    Console.WriteLine(item.completed);
+                }
+            }
+            else
+            {
+                Console.WriteLine(responce.StatusCode + " " + responce.ReasonPhrase);
+            }
+        }
+    }
+}
+
+
---
+++ HTTPClientDemo/HTTPClientFactoryDemo/Controllers/TodoController.cs
@@ -0,0 +1,38 @@
+﻿using HTTPClientFactoryDemo.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace HTTPClientFactoryDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TodoController : ControllerBase
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        public TodoController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetTodos()
+        {
+            var httpRequestMessage = new HttpRequestMessage(HttpMethod.Get, "https://jsonplaceholder.typicode.com/todos");
+
+            var httpClient = _httpClientFactory.CreateClient();
+            var httpResponceMessage = await httpClient.SendAsync(httpRequestMessage);
+
+            if(httpResponceMessage.IsSuccessStatusCode)
+            {
+                List<Todo> todo = JsonConvert.DeserializeObject<List<Todo>>(httpResponceMessage.Content.ReadAsStringAsync().Result);
+
+                return Ok(todo);
+            }
+            else
+            {
+                return NotFound(httpResponceMessage.StatusCode + " " + httpResponceMessage.ReasonPhrase);
+            }
+        }
+    }
+}
